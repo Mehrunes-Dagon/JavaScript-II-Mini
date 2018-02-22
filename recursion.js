af42ab034124ed513af100b088c9eb34c40ed544
William <wtpwinberg@gmail.com>
2018-02-22 12:41:02 -0800

    Complete recursion

--- recursion.js
+++ recursion.js
@@ -1,33 +1,37 @@
-// to test these problems you can run 'node recursion.js' in your terminal
-// Problem 1:
+// // to test these problems you can run 'node recursion.js' in your terminal
+// // Problem 1:
 
-let n = 1;
-while (n <= 10) {
-  console.log('While Loop', n);
-  n++;
-}
+// let n = 1;
+// while (n <= 10) {
+//   console.log('While Loop', n);
+//   n++;
+// }
 
-// write a recursive - function called countToTen that mimics the while loop above.
+// // write a recursive - function called countToTen that mimics the while loop above.
 
-// code here
+// // code here
 
-// when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
-/* ================ Next Problem ================= */
+// // when you code is ready, un-comment the next line and run the file
+// // console.log(countToTen());
+// /* ================ Next Problem ================= */
 
-// Problem 2:
+// // Problem 2:
 
-const factorial = n => {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
-};
+// const factorial = n => {
+//   let result = 1;
+//   for (let i = 2; i <= n; i++) {
+//     result *= i;
+//   }
+//   return result;
+// };
 
-console.log(factorial(5));
+// console.log(factorial(5));
 
 // write the above function in a recursive way.
-
+const factorDis = (n) => {
+  if (n === 0) return 1;
+  console.log(n);
+  return n * factorDis(n - 1);
+};
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(factorDis(0));
